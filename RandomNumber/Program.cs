b26889f0abf84860844589384c7fb95abd7ab067
desired <desired@DESKTOP-BNNTGOI>
2023-01-23 10:16:30 +0200

    Добавьте файлы проекта.

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random= new Random();
+        
+
+        while (true)
+        {
+            int value = random.Next(0,100);
+            Console.WriteLine(value);
+            Console.ReadKey();
+        }
+    }
+}
